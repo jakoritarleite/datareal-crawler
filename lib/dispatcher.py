fe22f48208b2946d84e6e99f6fa378b868a8e6ef
Joao Koritar <jakoritarleite@gmail.com>
2020-10-21 14:12:38 -0300

    fix some generated errors by Map

--- lib/dispatcher.py
+++ lib/dispatcher.py
@@ -4,9 +4,8 @@ import boto3
 sns = boto3.client('sns')
 
 class Dispatcher(object):
-    def __init__(self, topicArn, crawlId):
+    def __init__(self, topicArn):
         self.topicArn = topicArn
-        self.crawlId = crawlId
 
     def send_batch(self, jobs):
         print('Dispatcher.sendBatch()')
@@ -14,18 +13,17 @@ class Dispatcher(object):
         if not jobs:
             raise Exception('Missing jobs for Dispatcher.sendBatch')
 
-        return list(map(self.send, jobs))
+        return [ self.send(jobs[i]) for i in range(len(jobs)) ]
 
     def send(self, job):
         print('Dispatcher.send()')
-        print(job)
 
         if not job:
             raise Exception('Missing job for Dispatcher.send')
 
         response = sns.publish(
             TopicArn=self.topicArn,    
-            Message=str(job),    
+            Message=json.dumps(job),    
         )
         
         return response
