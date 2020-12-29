0580a0b73d5197939705b946e74a1069248b23c2
Joao Koritar <jakoritarleite@gmail.com>
2020-12-29 01:43:37 -0300

    Add items to be ignored when iterating the given dictionary.

---
+++ lib/sanitizer/settings.py
@@ -0,0 +1,17 @@
+IGNORE_ITEMS = [
+    'id',
+    'scrapeId',
+    'url',
+    'domain',
+    'date', 
+    'latitude', 
+    'longitude', 
+    'location', 
+    'address', 
+    'zipcode', 
+    'neighbourhood', 
+    'city',
+    'p_area',
+    'u_area',
+    't_area'
+]
