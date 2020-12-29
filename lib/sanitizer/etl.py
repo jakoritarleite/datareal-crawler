ef624cf99efb1f0e77c7c8bc68413cbe89c5ac8f
Joao Koritar <jakoritarleite@gmail.com>
2020-12-29 01:38:17 -0300

    Create function call by item name (string)
    It will call each dictionary item that won't be ignored and clean it's content.\nEg.: In a dict like the following:\n\nIt will call a function named title to clean the dict['title'] and the same for price and the rest.

--- lib/sanitizer/etl.py
+++ lib/sanitizer/etl.py
@@ -1,16 +1,24 @@
-from re import findall
-from lxml import etree
-from io import StringIO
+import importlib
+
+from lib.sanitizer import settings
 
 class Sanitizer:
     def __init__(self, content: dict, encoding: str = 'UTF-8'):
-        self.encoding = encoding
-        
+        assert isinstance(encoding, str), \
+            f'Encoding must be a String and not {type(encoding)}'
+
         assert isinstance(content, dict), \
             f'Content must be a Dictionary and not {type(content)}'
 
+        self.encoding = encoding
         self.content = content
 
     def clean(self):
-        pass
+        for item in self.content:
+            if self.content[item] and item not in settings.IGNORE_ITEMS:
+                function = getattr(importlib.import_module('lib.sanitizer.cleaners'), item)
+                self.content[item] = function(self.content[item])
 
+            else: pass
+
+        return self.content
